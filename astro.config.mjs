80680cf980cd383b6365eb76d6ab742af731e53d
tahzeer <tahzeer.ash@gmail.com>
2025-10-13 16:14:35 +0530

    fix: update astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,6 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  base: 'https://tahzeer.github.io',
   vite: {
     plugins: [tailwindcss()]
   }
