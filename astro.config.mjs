db86b0424ab1aa43e7653c21128ece218bbab230
tahzeer <tahzeer.ash@gmail.com>
2025-10-13 15:47:16 +0530

    fix: add base site ref in astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  base: '/',
+  site: 'https://tahzeer.github.io',
   vite: {
     plugins: [tailwindcss()]
   }
